7a5fe611b6846236c07de61702660e86e2629ff7
Davy Jones <davyinfrance@hotmail.com>
2016-07-23 09:50:12 +0200

    Trying to get space scoops to work again, changed default deploy for parachutes. plus other stuff

---
+++ PowerTriggers/Connection.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace PowerTriggers
+{
+    [KSPAddon(KSPAddon.Startup.MainMenu, true)]
+    public class Connection : MonoBehaviour
+    {
+        public Connection()
+        {
+        }
+
+        public void FixedUpdate()
+        {
+            // this is called many times, at regular intervals.
+        }
+    }
+}
---
+++ PowerTriggers/ElectricTriggerModule.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PowerTriggers
+{
+    public class ElectricTriggerModule : PartModule
+    {
+        [KSPEvent(guiActive =true, guiName = "Click Me!", active =true)]
+        public void ClickMe()
+        {
+            // event that fires from the ui part button
+        }
+
+        public override string GetInfo()
+        {
+            return "GE ElectricTrigger Module";
+        }
+    }
+}
